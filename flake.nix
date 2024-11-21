45583c8f463eef44969221835b75929528b2aef5
Chris Dunphy <chris@megaparsec.ca>
2024-11-20 21:50:41 -0700

    Add taskfile

--- flake.nix
+++ flake.nix
@@ -12,7 +12,13 @@
       in {
         devShell = pkgs.mkShell {
           # The packages we need for this project
-          buildInputs = with pkgs; [ go_1_23 go-tools gopls golangci-lint ];
+          buildInputs = with pkgs; [
+            go_1_23
+            go-tools
+            gopls
+            golangci-lint
+            go-task
+          ];
         };
       });
 }
